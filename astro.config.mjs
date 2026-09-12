fddd0d75c8091a9d732bbce6b4817cff0f0b6e24
ryanjcross <ryanc31415@gmail.com>
2026-09-12 13:07:41 -0600

    kwf

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://ryanjcross.com',
+  redirects: {
+    '/projects/stampede-sky': '/projects/sierra-space/',
+    '/projects/cu-boulder': '/projects/kashmir-world-foundation/',
+    '/projects/project-04': '/projects/design-build-fly/',
+  },
 });
